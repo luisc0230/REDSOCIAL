281336e3e1fbc096f823c157b3687ca0c68eaa4d
fabricl1905 <129562085+fabricl1905@users.noreply.github.com>
2023-06-09 12:24:55 -0500

    Update donaciones.sql
    Actualizacion de base datos

--- REDSOCIAL/donaciones.sql
+++ REDSOCIAL/donaciones.sql
@@ -17,13 +17,13 @@ CREATE TABLE `comentarios` (
 --
 
 INSERT INTO `comentarios` (`id`, `comentario`, `usuario`, `reply`, `fecha`) VALUES
-(22, 'Probando codigo', 1, 0, '2017-04-12 21:40:03'),
-(23, '@tuntoriales Hola tuntoriales', 1, 22, '2017-04-12 21:41:52'),
-(24, 'Probando codigo', 3, 0, '2017-04-12 21:42:14'),
-(25, '@tuntoriales Hola tuntoriales tambien jaja', 3, 22, '2017-04-12 21:42:27'),
-(26, '@bot Hola bot viejo amigo saludois', 2, 24, '2017-04-12 21:43:17'),
-(27, '@tuntoriales Jaja', 2, 22, '2017-04-12 21:43:23'),
-(28, ' Pikaaa man', 2, 0, '2017-04-12 21:43:28');
+(22, 'Hola en Av.Bolognesi una persona mayor pidiendo comida, por favor si pueden apoyar', 1, 0, '2017-04-12 21:40:03'),
+(23, '@Fabrizio Hola he visto que necesita una ayuda, me gustaria poder ayudarlo', 1, 22, '2017-04-12 21:41:52'),
+(24, 'Hola, por favor me ayudarian con medicinas para mi mascota, no tengo alcance ahora mismo para sus medicinas, esta muy grave.', 3, 0, '2017-04-12 21:42:14'),
+(25, 'Tambien hay un mayor de edad en Jr. de la Union si me pueden apoyar para ayudarlo', 3, 22, '2017-04-12 21:42:27'),
+(26, '@Fabrizio Buenas, cual es la medicina que necesitas.', 2, 24, '2017-04-12 21:43:17'),
+(27, '@Fabrizio Me gustaria apoyar', 2, 22, '2017-04-12 21:43:23'),
+(28, ' Hola, que tal por favor si pueden hacer donaciones para la gente afectada por los rios', 2, 0, '2017-04-12 21:43:28');
 
 -- --------------------------------------------------------
 
@@ -44,9 +44,10 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `usuario`, `contrasena`, `avatar`, `fecha_reg`) VALUES
-(1, 'tuntoriales', '4d186321c1a7f0f354b297e8914ab240', 'images/default.png', '2017-04-19'),
-(2, 'pikaman', '4d186321c1a7f0f354b297e8914ab240', 'images/pikabob.png', '2017-04-12'),
-(3, 'bot', '4d186321c1a7f0f354b297e8914ab240', 'images/pig.png', '2017-04-02');
+(1, 'Fabrizio', '4d186321c1a7f0f354b297e8914ab240', 'images/default.png', '2017-04-19'),
+(2, 'Luis', '4d186321c1a7f0f354b297e8914ab240', 'images/pikabob.png', '2017-04-12'),
+(3, 'Jhon', '4d186321c1a7f0f354b297e8914ab240', 'images/pig.png', '2017-04-02'),
+(4, 'Omar', '4d186321c1a7f0f354b297e8914ab240', 'images/pig.png', '2017-04-10');
 
 --
 -- Índices para tablas volcadas
