8226a861ee2f59c5f4c325c2bf6b2209c8b47204
Luis Adrian <122585598+luisc0230@users.noreply.github.com>
2023-06-30 18:51:25 -0500

    Update dashboard.php

--- REDSOCIAL/dashboard.php
+++ REDSOCIAL/dashboard.php
@@ -45,7 +45,7 @@ $totalComentarios = $rowComentarios['totalComentarios'];
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Dashboard con Bootstrap 5</title>
+    <title>Dashboard</title>
     <!-- Enlaces a los archivos CSS de Bootstrap, FontAwesome y Chart.js -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
